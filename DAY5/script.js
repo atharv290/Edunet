e2c4044555e958f8695168ed428557f043488184
atharv290 <atharvj938@gmail.com>
2025-10-08 11:03:12 +0530

    DAY 8 Updated

--- DAY5/script.js
+++ DAY5/script.js
@@ -1,1 +1,28 @@
-console.log("External JS file");
+//console.log("External JS file");
+function evenodd(){
+    let n = 9
+    let arr = [10,3,5,6,4,7,9,2,8];
+    for(let i=0;i<=n;i++){
+        if(arr[i]%2==0){
+            console.log(arr[i] + " is even");
+        }
+        else{
+            console.log(arr[i]+ " is odd");
+        }
+    }
+}
+//evenodd();
+function findStudent(){
+    const students = [
+        {name: "Atharva", age: 20},
+        {name: "Yash", age: 15},
+        {name: "Vaibhav", age: 12},
+        {name: "Adi", age: 17}
+    ]
+    students.forEach(student => {
+        if(student.age>15){
+            console.log(student.name );
+        }
+    });
+}
+findStudent();
---
+++ DAY8/Example.py
@@ -0,0 +1,9 @@
+num = int(input("Enter your marks: "))
+if num >80:
+    print("1st")
+elif num <=80 and num >=60:
+    print("2nd")
+elif num <60 and num >=40:
+    print("3rd")
+else:
+    print("Fail")
